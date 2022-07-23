05c13126b8fc7d291f5a537eeba4d0617ffcdc8e
RandomDude470 <yassir.daoudi.2003@gmail.com>
2022-07-23 17:19:18 +0100

    new file:   .gitignore new file:   Remove Facebook's annoying pip feature/17.png new file:   Remove Facebook's annoying pip feature/manifest.json new file:   Remove Facebook's annoying pip feature/script.js

---
+++ Remove Facebook's annoying pip feature/script.js
@@ -0,0 +1,8 @@
+(function() {
+    window.setInterval(function() {
+      var elem = document.querySelector('.rq0escxv.l9j0dhe7.du4w35lb .tqsryivl.k01i5q8h.eu4i7hue.j83agx80.esma6hys.taijpn5t.k3cnv2bo.poy2od1o.a9ndjppc');
+      if (elem) {
+      elem.remove();  
+      }
+    }, 500)
+  }())
